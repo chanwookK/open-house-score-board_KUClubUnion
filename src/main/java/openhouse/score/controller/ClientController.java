c370abf0500018c60b2ba83beef9a0138ec10035
kangchanwook <kan0202@naver.com>
2024-05-21 14:49:34 +0900

    대대적 변경 필요. 변경 전 커밋

--- src/main/java/openhouse/score/controller/ClientController.java
+++ src/main/java/openhouse/score/controller/ClientController.java
@@ -9,6 +9,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+/**
+ *
+ * Client 들에게 보이는 Main View를 위한 컨트롤러
+ *  URL Mapping : "/"
+ *  
+ */
+
 @Controller
 @RequiredArgsConstructor
 public class ClientController {
